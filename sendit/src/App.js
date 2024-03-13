6aa8f5a98a4d6339b3f8c086b43e90df0c0e9f95
Muenishikuku <charlesmueni2@gmail.com>
2024-03-13 12:24:45 +0300

    updated work

--- sendit/src/App.js
+++ sendit/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter as Router
+import LoginForm from './Components/LoginForm';
+import SignupForm from './Components/SignupForm';
+import Delivery from "./Components/Delivery";
+import Home from "./Home";
+import MapSearch from "./Components/MapSearch";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/signup" element={<SignupForm />} />
+                <Route path="/delivery" element={<Delivery/>} />
+                <Route path="/mapsearch" element={<MapSearch/>} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
